53b52a3b02c088657c84ed91ab2fdc3549eacf5e
maurosluciosprojetos <maurosluciosprojetos@gmail.com>
2024-10-05 17:58:44 -0300

    Docs Swagger e OpenAPI JSON adicionados

---
+++ src/main/java/br/com/healthdatainsights/app/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package br.com.healthdatainsights.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Health Data Insights API")
+                        .version("1.0")
+                        .description("API para gerenciamento de médicos, pacientes, consultas e receitas"));
+    }
+}
